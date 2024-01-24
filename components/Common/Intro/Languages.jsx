c4bcd16dcfb40c6597b54a6acb85408decb19e2c
harimbt <hari.prasath@montbleu.com>
2024-01-24 10:01:59 +0530

    new code

--- components/Common/Intro/Languages.jsx
+++ components/Common/Intro/Languages.jsx
@@ -2,28 +2,28 @@ import { Progress } from "antd"
 import { useEffect, useState } from "react"
 
 const Languages = () => {
-    const [urdu, setUrdu] = useState(0)
+    const [tamil, setTamil] = useState(0)
     const [english, setEnglish] = useState(0)
 
     useEffect(() => {
         const timer = setInterval(() => {
-            if (urdu < 99) {
-                setUrdu(prevCount => prevCount + 1);
+            if (tamil < 99) {
+                setTamil(prevCount => prevCount + 1);
             }
             if (english < 90) {
                 setEnglish(prevCount => prevCount + 1);
             }
-        }, 30);
+        }, 50);
 
         return () => clearInterval(timer);
-    }, [urdu, english])
+    }, [tamil, english])
     return (
         <div className="flex flex-col space-y-1 pt-6">
             <div className="flex flex-col gap-y-4">
                 <span className='text-Snow text-xs font-bold'>Languages</span>
                 <div className="flex flex-row items-center justify-center space-x-6">
                     <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#1fdf64" type="circle" percent={urdu} size={75} />
+                        <Progress strokeColor="#1fdf64" type="circle" percent={tamil} size={75} />
                         <span className='text-xs font-bold text-Snow'>Tamil</span>
                     </div>
                     <div className="flex flex-col items-center justify-center gap-y-2">
